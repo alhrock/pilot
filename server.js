3842761ca5ebd33d967b479de226e3d0187af4b7
alhrock <gchatzipapas@hotmail.com>
2015-09-29 02:46:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 
-var agenda = require('agenda')({ db: { address: 'mongodb://pilot:3buy5n59@@ds041563.mongolab.com:41563/heroku_lcsp70gh' } });
+var agenda = require('agenda')({ db: { address: '  mongodb://pilot:0000@ds051923.mongolab.com:51923/pilot' } });
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 var _ = require('lodash');
@@ -82,7 +82,7 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
-mongoose.connect(' mongodb://pilot:3buy5n59@@ds041563.mongolab.com:41563/heroku_lcsp70gh');
+mongoose.connect('  mongodb://pilot:0000@ds051923.mongolab.com:51923/pilot');
 
 var app = express();
 
